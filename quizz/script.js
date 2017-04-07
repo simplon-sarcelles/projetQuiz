e2d4cc4dabf4526a5a6402545898bf4ea2fdb01c
jonathanManet <ngoathan@yahoo.fr>
2017-04-07 15:26:19 +0200

    final commit

---
+++ quizz/script.js
@@ -0,0 +1,382 @@
+$(function(){
+
+$("#q1r1").click(function(){
+  $("div:eq(3),div:eq(4),div:eq(6),div:eq(7)").removeClass("vert rouge")
+$("#q1r1").addClass("rouge")
+});
+
+
+
+
+
+$("#q1r2").click(function(){
+$("div:eq(3),div:eq(4),div:eq(6),div:eq(7)").removeClass("vert rouge")
+$("#q1r2").addClass("vert")
+
+});
+
+$("#q1r3").click(function(){
+  $("div:eq(3),div:eq(4),div:eq(6),div:eq(7)").removeClass("vert rouge")
+$("#q1r3").addClass("rouge")
+
+});
+
+$("#q1r4").click(function(){
+  $("div:eq(3),div:eq(4),div:eq(6),div:eq(7)").removeClass("vert rouge")
+$("#q1r4").addClass("rouge")
+
+});
+//FIN QUESTION 1
+
+
+$("#q2r1").click(function(){
+  $("div:eq(10),div:eq(11),div:eq(13),div:eq(14)").removeClass("vert rouge")
+$("#q2r1").addClass("rouge")
+
+});
+
+$("#q2r2").click(function(){
+$("div:eq(10),div:eq(11),div:eq(13),div:eq(14)").removeClass("vert rouge")
+$("#q2r2").addClass("rouge")
+
+});
+
+$("#q2r3").click(function(){
+  $("div:eq(10),div:eq(11),div:eq(13),div:eq(14)").removeClass("vert rouge")
+$("#q2r3").addClass("vert")
+
+});
+
+$("#q2r4").click(function(){
+  $("div:eq(10),div:eq(11),div:eq(13),div:eq(14)").removeClass("vert rouge")
+$("#q2r4").addClass("rouge")
+
+});
+
+//FIN QUESTION 2
+
+
+$("#q3r1").click(function(){
+  $("div:eq(17),div:eq(18),div:eq(20),div:eq(21)").removeClass("vert rouge")
+$("#q3r1").addClass("rouge")
+
+});
+
+$("#q3r2").click(function(){
+$("div:eq(17),div:eq(18),div:eq(20),div:eq(21)").removeClass("vert rouge")
+$("#q3r2").addClass("rouge")
+
+});
+
+$("#q3r3").click(function(){
+  $("div:eq(17),div:eq(18),div:eq(20),div:eq(21)").removeClass("vert rouge")
+$("#q3r3").addClass("vert")
+
+});
+
+$("#q3r4").click(function(){
+  $("div:eq(17),div:eq(18),div:eq(20),div:eq(21)").removeClass("vert rouge")
+$("#q3r4").addClass("rouge")
+
+});
+
+//FIN QUESTION 3
+
+
+$("#q4r1").click(function(){
+  $("div:eq(24),div:eq(25),div:eq(27),div:eq(28)").removeClass("vert rouge")
+$("#q4r1").addClass("rouge")
+
+});
+
+$("#q4r2").click(function(){
+$("div:eq(24),div:eq(25),div:eq(27),div:eq(28)").removeClass("vert rouge")
+$("#q4r2").addClass("rouge")
+
+});
+
+$("#q4r3").click(function(){
+  $("div:eq(24),div:eq(25),div:eq(27),div:eq(28)").removeClass("vert rouge")
+$("#q4r3").addClass("rouge")
+
+});
+
+$("#q4r4").click(function(){
+  $("div:eq(24),div:eq(25),div:eq(27),div:eq(28)").removeClass("vert rouge")
+$("#q4r4").addClass("vert")
+
+});
+
+//FIN QUESTION 4
+
+$("#q5r1").click(function(){
+  $("div:eq(31),div:eq(32),div:eq(34),div:eq(35)").removeClass("vert rouge")
+$("#q5r1").addClass("rouge")
+
+});
+
+$("#q5r2").click(function(){
+$("div:eq(31),div:eq(32),div:eq(34),div:eq(35)").removeClass("vert rouge")
+$("#q5r2").addClass("rouge")
+
+});
+
+$("#q5r3").click(function(){
+  $("div:eq(31),div:eq(32),div:eq(34),div:eq(35)").removeClass("vert rouge")
+$("#q5r3").addClass("rouge")
+
+});
+
+$("#q5r4").click(function(){
+  $("div:eq(31),div:eq(32),div:eq(34),div:eq(35)").removeClass("vert rouge")
+$("#q5r4").addClass("vert")
+
+});
+
+//FIN QUESTION 5
+
+$("#q6r1").click(function(){
+  $("div:eq(38),div:eq(39),div:eq(41),div:eq(42)").removeClass("vert rouge")
+$("#q6r1").addClass("vert")
+
+});
+
+$("#q6r2").click(function(){
+$("div:eq(38),div:eq(39),div:eq(41),div:eq(42)").removeClass("vert rouge")
+$("#q6r2").addClass("rouge")
+
+});
+
+$("#q6r3").click(function(){
+  $("div:eq(38),div:eq(39),div:eq(41),div:eq(42)").removeClass("vert rouge")
+$("#q6r3").addClass("rouge")
+
+});
+
+$("#q6r4").click(function(){
+  $("div:eq(38),div:eq(39),div:eq(41),div:eq(42)").removeClass("vert rouge")
+$("#q6r4").addClass("rouge")
+
+});
+
+//FIN QUESTION 6
+
+$("#q7r1").click(function(){
+  $("div:eq(45),div:eq(46),div:eq(48),div:eq(49)").removeClass("vert rouge")
+$("#q7r1").addClass("rouge")
+
+});
+
+$("#q7r2").click(function(){
+$("div:eq(45),div:eq(46),div:eq(48),div:eq(49)").removeClass("vert rouge")
+$("#q7r2").addClass("rouge")
+
+});
+
+$("#q7r3").click(function(){
+  $("div:eq(45),div:eq(46),div:eq(48),div:eq(49)").removeClass("vert rouge")
+$("#q7r3").addClass("vert")
+
+});
+
+$("#q7r4").click(function(){
+  $("div:eq(45),div:eq(46),div:eq(48),div:eq(49)").removeClass("vert rouge")
+$("#q7r4").addClass("rouge")
+
+});
+
+//FIN QUESTION 7
+
+$("#q8r1").click(function(){
+  $("div:eq(52),div:eq(53),div:eq(55),div:eq(56)").removeClass("vert rouge")
+$("#q8r1").addClass("rouge")
+
+});
+
+$("#q8r2").click(function(){
+$("div:eq(52),div:eq(53),div:eq(55),div:eq(56)").removeClass("vert rouge")
+$("#q8r2").addClass("vert")
+
+});
+
+$("#q8r3").click(function(){
+  $("div:eq(52),div:eq(53),div:eq(55),div:eq(56)").removeClass("vert rouge")
+$("#q8r3").addClass("rouge")
+
+});
+
+$("#q8r4").click(function(){
+  $("div:eq(52),div:eq(53),div:eq(55),div:eq(56)").removeClass("vert rouge")
+$("#q8r4").addClass("rouge")
+
+});
+
+//FIN QUESTION 8
+
+$("#q9r1").click(function(){
+  $("div:eq(59),div:eq(60),div:eq(62),div:eq(63)").removeClass("vert rouge")
+$("#q9r1").addClass("rouge")
+
+});
+
+$("#q9r2").click(function(){
+$("div:eq(59),div:eq(60),div:eq(62),div:eq(63)").removeClass("vert rouge")
+$("#q9r2").addClass("rouge")
+
+});
+
+$("#q9r3").click(function(){
+  $("div:eq(59),div:eq(60),div:eq(62),div:eq(63)").removeClass("vert rouge")
+$("#q9r3").addClass("rouge")
+
+});
+
+$("#q9r4").click(function(){
+  $("div:eq(59),div:eq(60),div:eq(62),div:eq(63)").removeClass("vert rouge")
+$("#q9r4").addClass("vert")
+
+});
+
+//FIN QUESTION 9
+
+$("#q10r1").click(function(){
+  $("div:eq(66),div:eq(67),div:eq(69),div:eq(70)").removeClass("vert rouge")
+$("#q10r1").addClass("rouge")
+
+});
+
+$("#q10r2").click(function(){
+$("div:eq(66),div:eq(67),div:eq(69),div:eq(70)").removeClass("vert rouge")
+$("#q10r2").addClass("rouge")
+
+});
+
+$("#q10r3").click(function(){
+  $("div:eq(66),div:eq(67),div:eq(69),div:eq(70)").removeClass("vert rouge")
+$("#q10r3").addClass("rouge")
+
+});
+
+$("#q10r4").click(function(){
+  $("div:eq(66),div:eq(67),div:eq(69),div:eq(70)").removeClass("vert rouge")
+$("#q10r4").addClass("vert")
+
+});
+
+//FIN QUESTION 10
+
+
+
+var compteur = 0;
+
+
+var $currDiv = $( ".startPrev" );
+$( "#previous" ).click(function() {
+  $currDiv  = $currDiv .prev();
+  $( ".question" ).hide();
+  $currDiv .show();
+});
+
+var $currDiv = $( ".start" );
+$currDiv .show();
+$( "#next" ).click(function() {
+  $currDiv  = $currDiv .next();
+  $( ".question" ).hide();
+  $currDiv .show();
+});
+
+
+
+
+
+
+$(".next").click(function(){
+
+compteur ++;
+$(".previous").show();
+console.log(compteur);
+if (compteur===10) {
+  $(".next").hide()
+}else if (compteur===0) {
+  $(".previous").hide()
+}
+
+});
+
+$(".previous").click(function(){
+
+compteur--;
+console.log(compteur);
+if (compteur===9) {
+$(".next").show()
+}else if (compteur===0) {
+  $(".previous").hide()
+}
+});
+
+
+
+if (compteur===0) {
+$(".previous").hide()
+}
+
+
+
+$("#bouton").click(function(){
+$("#bouton").hide();
+$(".fond").animate({"top":"-100%"})
+$(".debut").removeClass("debut").css("transition-duration","0.7s");
+
+  var audio = $("#fondmusique")[0];
+  audio.play();
+
+
+
+});
+
+
+
+ var time = 0 ;
+
+$("#time").html(time+" Seconde");
+
+$("#bouton").click(function(){
+
+setInterval(function(){
+
+  time++
+  $("#time").html(time+" Secondes");
+
+if (time === 60) {
+
+$(location).attr("href",'fin.html')
+
+}
+
+}, 1000);
+
+});
+
+var faute = 0 ;
+
+$(".mauvais").one("click",function(){
+  faute ++;
+  $("#faute").html("Faute:"+faute);
+    var audio = $("#mauvais")[0];
+    audio.play();
+
+});
+
+
+
+$(".bon").one("click",function(){
+
+  var audio = $("#bon")[0]  ;
+  audio.play();
+
+});
+
+$("#faute").html("Faute:"+faute);
+
+
+});
